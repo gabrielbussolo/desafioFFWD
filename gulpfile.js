20ad4de1093c4af826920afb387d8db6ae403736
Gabriel Ferreira <gabreeiel@gmail.com>
2018-04-30 13:52:23 -0300

    v0.1 -  Commit inicial, estrutura de diretorios, npm init, gulp config.

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const uglify = require('gulp-uglify')
+const concat = require('gulp-concat')
+const browserSync = require('browser-sync').create();
+
+/* TASKS */
+
+/*
+  Copy all .html
+*/
+gulp.task('copyHTML', () => {
+  gulp.src('src/*.html')
+    .pipe(gulp.dest('public'))
+    .pipe(browserSync.stream())
+})
+
+/*
+  Compile and minify sass
+  lib: https://github.com/dlmanning/gulp-sass
+*/
+gulp.task('sassCompileMinify', () => {
+  gulp.src('src/assets/sass/*.scss')
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(gulp.dest('public/css'))
+    .pipe(browserSync.stream())
+})
+
+/*
+  Concat and Uglify JS
+  libs: https://github.com/gulp-community/gulp-concat
+        https://github.com/terinjokes/gulp-uglify
+*/
+gulp.task('jsConcatUglify', ()=>{
+  gulp.src('assets/js/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('public/js'))
+    .pipe(browserSync.stream())
+})
+
+/*
+  Browser Sync
+  doc: https://browsersync.io/docs/gulp
+*/
+gulp.task('serve', ['copyHTML', 'sassCompileMinify', 'jsConcatUglify'], ()=>{
+  browserSync.init({
+    server: './public'
+  })
+
+  gulp.watch('src/*.html', ['copyHTML'])
+  gulp.watch('src/assets/sass/*', ['sassCompileMinify'])
+  gulp.watch('src/assets/js/*', ['jsConcatUglify'])
+})
+
+// Default  task
+gulp.task('default', ['copyHTML', 'sassCompileMinify', 'jsConcatUglify'])
